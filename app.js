d17dc1b9a9b7e9316bbacef340fbe1a9c338cc79
DyS29 <deparra12@soy.sena.edu.com>
2024-06-16 00:38:56 -0500

    Se creo ruta para resgistro , listar usuarios y eliminar, y se modulo el proyecto

--- app.js
+++ app.js
@@ -2,53 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 // Get the client
-const mysql = require('mysql2/promise');
+
 const cors = require('cors')
 const session = require('express-session')
+const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const login = require('./login');
+const registro = require('./registro');
+const { eliminarUsuario, obtenerUsuario } = require('./usuario');
+const validar = require('./validar');
+const saltRounds = 10;
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin: 'http://localhost:5173',
   credentials: true
 }))
-app.use(session( {  
-  secret:'saksasd8sa7d7sad'
+app.use(session({
+  secret: 'saksasd8sa7d7sad'
 }))
 // Create the connection to database
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'login',
-});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/login',async(req, res) => {
-  const datos=req.query;
-  try {
-    const [results, fields] = await connection.query(
-      "SELECT * FROM `usuario` WHERE `usuario` = ? AND `clave` = ?",
-      [datos.usuario,datos.clave]
-    );
-    if(results.length > 0) {
-      req.session.usuario = datos.usuario;
-      res.status(200).send('Inicio de sesion correcto')
-    } else {
-      res.status (401).send ('Datos incorrecto')
-    }                                                                                                                                                       
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  } catch (err) {
-    console.log(err);
-  }
- })
-app.get('/validar', (req, res) => {
-  if(req.session.usuario){
-    res.status(200).send( 'sesion valida')
-  } else {
-    res.status(401).send( 'No autorizado')}
-})
+app.get('/login', login)
 
-app.listen(port, () => {                              
+app.get('/validar', validar)
+
+app.get('/registro', registro)
+
+app.get('/usuarios', obtenerUsuario)
+
+app.delete('/usuarios', eliminarUsuario)
+
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ validar.js
@@ -0,0 +1,8 @@
+const validar = (req, res) => {
+    if (req.session.usuario) {
+        res.status(200).send('sesion valida')
+    } else {
+        res.status(401).send('No autorizado')
+    }
+}
+module.exports = validar 
